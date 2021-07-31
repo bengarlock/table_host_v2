d98f3bc1525237fb88800da0c7d543a286c69bf0
bengarlock <ben.garlock@gmail.com>
2021-07-31 15:47:28 -0400

    got nav buttons lookin good in mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,19 +9,18 @@ class Navbar extends React.Component {
     render() {
         return(
             <div id="nav-bar-wrapper">
-                <span>
-                    ICON
-                </span>
-                <span>
-                    ICON
-                </span><span>
-                    ICON
-                </span>
-                <span>
-                    ICON
-                </span>
+                <span className="menu-icon">BOOK</span>
 
 
+                <span className="menu-icon">
+                    FLOOR
+                </span >
+                <span className="menu-icon">
+                    GUESTS
+                </span>
+                <span className="menu-icon">
+                    REPORTS
+                </span>
 
                 {/*<NavLink className="menu-item" to="/" exact>Book</NavLink>*/}
                 {/*<NavLink className="menu-item" to="/floor" exact >Floor</NavLink>*/}
