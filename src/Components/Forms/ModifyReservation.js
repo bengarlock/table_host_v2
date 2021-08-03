b3789e37e66e2a0b3bf67e6eb623efa6e0c1e2be
github username <github email address>
2021-08-03 19:22:00 -0400

    sync commit

---
+++ src/Components/Forms/ModifyReservation.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import "../../Stylesheets/ReservationForm.css"
+import {connect} from "react-redux";
+import {changeSlot} from "../../Actions/Slot";
+import PropTypes from "prop-types";
+
+class ModifyReservation extends React.Component {
+
+    state = {
+        booked: '',
+        first_name: '',
+        last_name: '',
+        phone_number: '',
+        time: '',
+        party_size: '',
+        status: '',
+        reservation_notes: '',
+        tables: '',
+    }
+
+    static propTypes = {
+        currentSlot: PropTypes.array.isRequired
+    }
+
+    onClickHandler = (e) => {
+        if (e.target.id === "overlay") {
+            this.props.changeSlot([])
+        }
+    }
+
+    onChangeHandler = (e) => {
+        if (e.target.name === "first-name") {
+            this.setState({
+                first_name: e.target.value
+            })
+        } else if (e.target.name === "last-name") {
+            this.setState({
+                last_name: e.target.value
+            })
+        } else if (e.target.name === "phone-number") {
+            this.setState({
+                phone_number: e.target.value
+            })
+        }
+    }
+
+    render() {
+        return(
+            <>
+                <div className="overlay" id="overlay" onClick={this.onClickHandler} />
+                <div className="form-wrapper">
+                    <h3>Reservation</h3>
+
+                    <form className="user-form">
+                        <input name="first-name"
+                               value={this.state.first_name}
+                               onChange={this.onChangeHandler}
+                               type="text"
+                               placeholder="First Name"/>
+                        <input name="last-name"
+                               value={this.state.last_name}
+                               onChange={this.onChangeHandler}
+                               type="text"
+                               placeholder="Last Name" />
+                        <input name="phone-number"
+                               value={this.state.phone_number}
+                               onChange={this.onChangeHandler}
+                               type="text"
+                               placeholder="Phone Number" />
+                        <input type="submit" />
+                    </form>
+                </div>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    currentSlot: state.book.currentBook,
+})
+
+
+export default connect(mapStateToProps, { changeSlot })(ModifyReservation);
--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../Stylesheets/ReservationForm.css"
+import ModifyReservation from "./Forms/ModifyReservation";
+import NewReservation from "./Forms/NewReservation";
 import { connect } from "react-redux";
 import { changeSlot } from "../Actions/Slot";
 import PropTypes from "prop-types";
@@ -7,39 +9,25 @@ import PropTypes from "prop-types";
 
 class ReservationForm extends React.Component {
 
+
+
     static propTypes = {
         currentSlot: PropTypes.array.isRequired
     }
 
-    onClickHandler = (e) => {
-        if (e.target.id === "overlay") {
-            this.props.changeSlot([])
-        }
+    componentDidMount = () => {
+
     }
+
+
+
+
 
 
     render() {
         return(
             <div>
-                {this.props.currentSlot ?
-                    <div>
-                        <div className="overlay" id="overlay" onClick={this.onClickHandler} />
-                        <div className="form-wrapper">
-                            <h3>Reservation</h3>
-                            <form className="user-form">
-                                <input name="first-name" type="text" placeholder="First Name"/>
-                                <input name="last-name" type="text" placeholder="Last Name" />
-
-
-                                <input name="phone-number" type="text" placeholder="Phone Number" />
-                                <input type="submit" />
-                            </form>
-                        </div>
-                    </div>
-
-                :
-                    null
-                }
+                {this.props.currentSlot[0].booked ? <ModifyReservation /> : <NewReservation />}
             </div>
         )
     }
