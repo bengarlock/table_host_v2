85445346a89e2621f24247ed03e2ffbe0aef8939
bengarlock <ben.garlock@gmail.com>
2021-09-12 07:47:42 -0400

    Reservations rendering on Floor View.

--- src/Components/Floor.js
+++ src/Components/Floor.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import "../Stylesheets/Floor.css"
+import { connect } from "react-redux";
+import PropTypes from "prop-types"
+import Slot from "./Slot";
 
 class Floor extends React.Component {
 
-    render() {
+    static propTypes = {
+        currentBook: PropTypes.array.isRequired
+    }
 
-        return(<div>FLOOR</div>)
+    renderReservations = () => {
+        if (this.props.currentBook[0]) {
+            const reservations = this.props.currentBook[0].slots.filter(reservation => reservation.booked)
+            return reservations.map(slot => <Slot key={slot.id} slot={slot}/>)
+        }
+
+    }
+
+    render() {
+        return(
+            <div className="floor-wrapper">
+                <div className="floor-reservations-wrapper" style={{width: "25vw"}}>
+                    <div className="floor-reservations-header">Reservations</div>
+                    <div>
+                        {this.renderReservations()}
+                    </div>
+                </div>
+                <div className="floor-tables-wrapper">
+
+                </div>
+            </div>
+        )
     }
 }
 
-export default Floor
+const mapStateToProps = (state) => ({
+    currentBook: state.book.currentBook
+})
+
+export default connect(mapStateToProps)(Floor)
