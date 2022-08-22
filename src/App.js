e871423d33749e3555ba44e78e89cdcd65b912f9
Ben Garlock <66751258+bengarlock@users.noreply.github.com>
2022-08-22 16:07:00 -0400

    resize floor plan with backend save

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
 
   render() {
-      // console.log(this.props.currentBook)
+      console.log(this.props.currentBook)
       return (
           <div className="app-wrapper">
               <Datepicker />
